9382fa518438193e2d6de1678ddb531a422b1d47
Jacob <jacob.varner14@gmail.com>
2016-01-15 12:57:38 -0600

    Added simple form to enter all of Auburn and opponent team stats for a game.

--- auburnbasketballstats.js
+++ auburnbasketballstats.js
@@ -1,17 +1,48 @@
+GamesList = new Mongo.Collection('games');
+PlayersList = new Mongo.Collection('players');
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+  Template.addGame.events({
+    'submit form': function(event){
+      event.preventDefault();
+      var gameDate = event.target.gameDate.value;
+      var gameLocation = event.target.gameLocation.value;
+      var gameOpponent = event.target.gameOpponent.value;
+      var gameAttendance = event.target.gameAttendance.value;
+      var gameAuburnScore =  event.target.gameAuburnScore.value;
+      var gameOpponentScore = event.target.gameOpponentScore.value;
+      var gameAuburnPOT = event.target.gameAuburnPOT.value;
+      var gameOpponentPOT = event.target.gameOpponentPOT.value;
+      var gameAuburnPIP = event.target.gameAuburnPIP.value;
+      var gameOpponentPIP = event.target.gameOpponentPIP.value;
+      var gameAuburn2CP = event.target.gameAuburn2CP.value;
+      var gameOpponent2CP = event.target.gameOpponent2CP.value;
+      var gameAuburnFBP = event.target.gameAuburnFBP.value;
+      var gameOpponentFBP = event.target.gameOpponentFBP.value;
+      var gameAuburnBP = event.target.gameAuburnBP.value;
+      var gameOpponentBP = event.target.gameOpponentBP.value;
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+      GamesList.insert({
+        date: gameDate,
+        location: gameLocation,
+        opponent: gameOpponent,
+        attendance: parseInt(gameAttendance),
+        auburnScore: parseInt(gameAuburnScore),
+        opponentScore: parseInt(gameOpponentScore),
+        auburnPOT: parseInt(gameAuburnPOT),
+        opponentPOT: parseInt(gameOpponentPOT),
+        auburnPIP: parseInt(gameAuburnPIP),
+        opponentPIP: parseInt(gameOpponentPIP),
+        auburn2CP: parseInt(gameAuburn2CP),
+        opponent2CP: parseInt(gameOpponent2CP),
+        auburnFBP: parseInt(gameAuburnFBP),
+        opponentFBP: parseInt(gameOpponentFBP),
+        auburnBP: parseInt(gameAuburnBP),
+        opponentBP: parseInt(gameOpponentBP)
+      });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+      var form = event.target;
+      form.reset();
     }
   });
 }
