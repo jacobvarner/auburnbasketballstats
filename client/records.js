5220f2e90220d7e9bf6852c9457eb37dda509b8e
Jacob <jacob.varner14@gmail.com>
2016-02-04 21:26:51 -0600

    Finished records page. Just needs to be styled later along with everything else.

--- client/records.js
+++ client/records.js
@@ -1,9 +1,27 @@
 Router.route('/records');
+Session.set('category', "Points");
 
 Template.records.events({
-
+  'change #category-select': function() {
+    Session.set('category', $('#category-select').val());
+  }
 });
 
 Template.records.helpers({
-
+  'categories': function() {
+    var categories = RecordInfo.find({}, {fields: {category: 1}}).fetch();
+    categories = _.pluck(categories, 'category');
+    var uniqueCategories = _.uniq(categories, true);
+    return uniqueCategories;
+  },
+  'career': function() {
+    var category = Session.get('category');
+    var output = RecordInfo.find({category: category, duration: "Career"}, {sort: {rank: 1}}).fetch();
+    return output;
+  },
+  'season': function() {
+    var category = Session.get('category');
+    var output = RecordInfo.find({category: category, duration: "Season"}, {sort: {rank: 1}}).fetch();
+    return output;
+  }
 });
